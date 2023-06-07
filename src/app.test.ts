d7d323a4ca1aaf70d21798ee7c92bcda65baa22e
Carlos Martínez <carlos.martinez@biko2.com>
2023-06-07 19:23:18 +0200

    adds test api memes endpoint returns a list

--- src/app.test.ts
+++ src/app.test.ts
@@ -5,4 +5,14 @@ describe("/api/memes", () => {
   it("endpoint exists", (done) => {
     request(app).get("/api/memes").expect(200, done);
   });
+
+  it("returns a list", (done) => {
+    request(app)
+      .get("/api/memes")
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toBeInstanceOf(Array);
+        done();
+      });
+  });
 });
