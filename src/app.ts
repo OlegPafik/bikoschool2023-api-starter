f3c8453e8534df3083d424d3b4d9ddab0e32d8cc
Carlos Martínez <carlos.martinez@biko2.com>
2023-06-07 20:03:52 +0200

    removes morgan logs from test environment

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,9 @@ export const createApp = (db: LowdbSync<DatabaseSchema>) => {
   const app: Express = express();
   // Shows request log on terminal
   // https://github.com/expressjs/morgan
-  app.use(morgan("dev"));
+  if (process.env.NODE_ENV !== "test") {
+    app.use(morgan("dev"));
+  }
   // Parses incoming requests with JSON payloads
   // http://expressjs.com/es/api.html#express.json
   app.use(express.json());
