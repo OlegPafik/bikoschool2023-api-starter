48474691936ce32c9cd4f5c65927917aa3dced5b
Carlos Martínez <carlos.martinez@biko2.com>
2023-06-07 19:35:26 +0200

    api memes returns a list of 50 memes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,20 @@
 import { Request, Response, Router } from "express";
+import lowdb from "lowdb";
+import FileSync from "lowdb/adapters/FileSync";
 
 export const routes = Router();
 
+interface DatabaseSchema {
+  memes: Meme[];
+}
+
+interface Meme {}
+
+const adapter = new FileSync<DatabaseSchema>("./data/db.json");
+const db = lowdb(adapter);
+
 routes.get("/memes", (req: Request, res: Response) => {
-  res.status(200).json([]);
+  const memes = db.get("memes").take(50).value();
+
+  res.status(200).json(memes);
 });
