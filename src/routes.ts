6be8ba88a3cedee89e5d7ccc168e6d7b93f7e36f
Oleg <oleg.pafik@eversens.com>
2023-06-10 12:38:59 +0200

    feat(routes): Return an array of 50 elements in GET /api/memes

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,8 @@ const routes = Router();
 // GET /api/memes
 routes.get('/memes', (req, res, next) => {
   //res.send([]); https://medium.com/gist-for-js/use-of-res-json-vs-res-send-vs-res-end-in-express-b50688c0cddf
-  res.json([]);
+  const array = Array(50);
+  res.json(array);
   res.sendStatus(200);
   })
 
