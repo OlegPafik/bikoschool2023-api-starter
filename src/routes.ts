645377e55b642126e51593fd3afedc75acdd5532
Oleg <oleg.pafik@eversens.com>
2023-06-10 11:46:18 +0200

    docs(routes): Add reference to difference between res.send and res.json

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ const routes = Router();
 
 // GET /api/memes
 routes.get('/memes', (req, res, next) => {
-  //res.send([]);
+  //res.send([]); https://medium.com/gist-for-js/use-of-res-json-vs-res-send-vs-res-end-in-express-b50688c0cddf
   res.json([]);
   res.sendStatus(200);
   })
